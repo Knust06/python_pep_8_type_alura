efefea765c0def2b2cd66aadacb3f628b04c6615
Knust06 <lucasknust0605@gmail.com>
2024-05-13 13:29:46 -0300

    Usando flake8 na main.py para ver se o codigo segue o pep8 e mypy.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from fila_normal import filanormal
+from fila_normal import FilaNormal
 from fila_prioritaria import FilaPrioritaria
 
-#ila_teste = filanormal()
-#fila_teste.atualizafila()
-#fila_teste.atualizafila()
-#fila_teste.atualizafila()
-#print(fila_teste.chamacliente(5))
-#print(fila_teste.chamacliente(10))
+# fila_teste = filanormal()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# print(fila_teste.chamacliente(5))
+# print(fila_teste.chamacliente(10))
 
 fila_teste_2 = FilaPrioritaria()
 fila_teste_2.atualiza_fila()
